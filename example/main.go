c94858bf6c46b0e2e2e77083fd0342a6f1467a13
Taru Karttunen <taruti@taruti.net>
2016-04-09 22:06:47 +0300

    Listen on localhost in the example

--- example/main.go
+++ example/main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	go RunServerHighLevel("127.0.0.1:2023", synthetic{})
-	go RunServerLowLevel("10.1.1.64:2024", readOnlyDirFs{})
+	go RunServerLowLevel("127.0.0.1:2024", readOnlyDirFs{})
 	<-make(chan int)
 }
